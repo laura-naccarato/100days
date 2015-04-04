beb9ab05e151952b17fa6cc783d44b12fbc3108c
laura-naccarato <laura.naccarato@gmail.com>
2015-04-04 12:06:46 -0400

    added daily post

--- page-home.php
+++ page-home.php
@@ -14,6 +14,24 @@ get_header();  ?>
       <p class="right">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam excepturi nemo ducimus ipsum eveniet veniam aspernatur facilis molestiae sapiente, odio ex molestias, minus alias provident consectetur quod corrupti soluta est in numquam illo animi ut perspiciatis unde. Nisi fugiat nemo, facere. Officia, amet modi possimus. Enim magnam consequatur at voluptates.</p> 
     </div> <!-- /.container -->
   </section>
+  <section class="days">
+    <div class="content">
+    <?php
+      query_posts( array( 'post_type' => 'day') );
+      if ( have_posts() ) : while ( have_posts() ) : the_post();?>
+      <div class="dailyPost"> 
+        <?php   the_post_thumbnail("large"); ?>
+        <div class="dailyPostContent"> 
+        <p class="technologies">  <?php the_terms($post->ID, "technologies", "", ", "); ?></p>
+
+        <h3><?php the_title(); ?></h3>
+        <?php the_content(); ?>
+          <a href="<?php  the_field('portfolio_url'); ?>" class="portfolioLink" target="_blank">Visit the full page</a>
+        </div>
+      </div>
+              <?php endwhile; endif; wp_reset_query(); ?>
+              </div>
+  </section>
 
 
 
